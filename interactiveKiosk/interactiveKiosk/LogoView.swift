45152f9d724e7c6184e3c20a1d6c5690054de433
kimjaewoo522 <kimjaewoo522@gmail.com>
2025-04-08 21:48:23 +0900

    logoView구현

--- interactiveKiosk/interactiveKiosk/LogoView.swift
+++ interactiveKiosk/interactiveKiosk/LogoView.swift
@@ -1,13 +1,35 @@
-//
-//  LogoView.swift
-//  interactiveKiosk
-//
-//  Created by ByonJoonYoung on 4/8/25.
-//
-
 import UIKit
 import SnapKit
 
 class LogoView: UIView {
 
+    private let logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "logo")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    private func setupView() {
+            self.addSubview(logoImageView)
+
+            logoImageView.snp.makeConstraints { make in
+                make.top.equalToSuperview().inset(12)      // 상단 여백 (상태바 고려)
+                make.leading.equalToSuperview().inset(1)   // 왼쪽 여백
+                make.height.equalTo(30)                    // 적절한 높이
+                make.width.lessThanOrEqualTo(120)          // 너무 커지지 않도록 제한
+            }
+        }
+   
 }
+
